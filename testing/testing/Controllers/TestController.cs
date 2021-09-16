957bb19874a2667b0dd08a557774c62346ddce29
Mounika612 <mounikamuni99@gmail.com>
2021-09-16 17:29:46 +0530

    first commit

---
+++ testing/testing/Controllers/TestController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using testing.Models;
+
+namespace testing.Controllers
+{
+    [RoutePrefix("hi")]
+    public class TestController : ApiController
+    {
+        [Route("hello")]
+        [HttpPost]
+        public output testfunction(input j)
+        {
+           
+            output temp = new output();
+            SampleData data = new SampleData();
+
+                if (j.inlet == "Hi")
+                {
+                temp.p1 = 123;
+                temp.p2 = "Abc";
+                List<SampleData> sampleList = new List<SampleData>();
+                sampleList.Add(new SampleData { p1p1=123, p1p2= "AbC" });
+               // sampleList.Add(new SampleData {});
+                sampleList.Add(new SampleData { p1p1 =123, p1p2 ="xyz" });
+                temp.p3 = sampleList;
+               }
+           
+            return temp;
+              
+        }
+       
+
+
+    }
+}
---
+++ testing/testing/Models/output.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testing.Models
+{
+    public class output
+    {
+        public int p1 { get; set; }
+        public string p2 { get; set; }
+        public List<SampleData> p3 { get; set; }
+    }
+
+    public class SampleData
+    {
+        public int p1p1 { get; set; }
+        public string p1p2 { get; set; }
+    }
+}
